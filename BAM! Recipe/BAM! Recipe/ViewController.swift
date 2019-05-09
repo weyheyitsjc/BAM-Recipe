8671d7dc446c89dccffd963a31230bd7bdd6e01c
weyheyitsle <49292600+WayHeyItsLe@users.noreply.github.com>
2019-05-09 18:22:40 -0400

    Add files via upload

---
+++ BAM! Recipe/BAM! Recipe/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  BAM! Recipe
+//
+//  Created by Le Trinh on 4/10/19.
+//  Copyright © 2019 Le Trinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //@IBOutlet var mainButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+       // mainButton.backgroundColor = UIColor.white
+       // mainButton.layer.cornerRadius = mainButton.frame.height / 2
+       // mainButton.setTitleColor(UIColor.blue, for: .normal)
+       
+      //  mainButton.layer.shadowRadius = 2
+       // mainButton.layer.shadowOpacity = 0.5
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
